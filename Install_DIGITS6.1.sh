f669bc8921fdd786e5e8625cb107e85423ef3be3
jiangwx <christophebigbear@163.com>
2018-12-03 23:03:14 +0800

    fix DIGITS

--- Install_DIGITS6.1.sh
+++ Install_DIGITS6.1.sh
@@ -1,5 +1,4 @@
-unzip digits.zip
-rm digits.zip
-mv DIGITS-master digits
-sudo pip install -r ~/digits/requirements.txt
-sudo pip install -e ~/digits
+git clone https://github.com/NVIDIA/DIGITS.git
+mv DIGITS digits
+sudo pip install -r digits/requirements.txt
+sudo pip install -e digits
