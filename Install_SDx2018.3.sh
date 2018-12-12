d696f72de54678f1f70e8e6c298f388cc3332ff6
jiangwx <christophebigbear@163.com>
2018-12-12 23:31:40 +0800

    add SDx2018.3

--- Install_SDx2018.3.sh
+++ Install_SDx2018.3.sh
@@ -1,10 +1,10 @@
 sudo rm /etc/default/grub
 sudo cp ./grub /etc/default/grub
 sudo grub-mkconfig -o /boot/grub/grub.cfg
-sudo mkdir /opt/Xilinx
+sudo mkdir /opt/Xilinx/
 cd /opt
 sudo chown $(whoami):users Xilinx/ -R
-cd /media/$(whoami)/Document/Xilinx/SDSoC/2018.2
+cd /media/$(whoami)/Document/Xilinx/SDSoC/2018.3
 sudo apt-get install ocl-icd-libopencl1 opencl-headers ocl-icd-opencl-dev linux-libc-dev g++ gcc gdb make libopencv-dev libjpeg-dev libpng-dev python git dmidecode lsb unzip -y
 sudo chmod +x xsetup
 sudo ./xsetup
