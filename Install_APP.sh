cb43549d49f1793fdf260873dabfd8d5025fb2e3
jiangwx <christophebigbear@163.com>
2018-12-12 19:31:40 +0800

    add docker and git

--- Install_APP.sh
+++ Install_APP.sh
@@ -37,4 +37,10 @@ sudo apt-get update
 sudo apt-get install notepadqq -y
 sudo apt-get install numix-gtk-theme numix-icon-theme-circle -y
 
-sudo pip install torch torchvision
+sudo apt-get install git -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce -y
