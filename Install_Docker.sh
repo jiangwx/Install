ffe846fa7c2d3783deb564a6c3f59c2d3b5a3611
jiangwx <christophebigbear@163.com>
2019-01-26 21:44:29 +0800

    fix docker install

--- Install_Docker.sh
+++ Install_Docker.sh
@@ -6,8 +6,7 @@ sudo apt update
 sudo apt install docker-ce -y
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
-  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 sudo apt-get update
 sudo apt-get install -y nvidia-docker2
 sudo pkill -SIGHUP dockerd
